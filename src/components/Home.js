0c95f331e29fe3007cf03701b82a82bf7f13e55b
vbrodar <vedran.brodar@hotmail.com>
2020-11-06 11:19:40 +0100

    Modified Home design

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,24 @@ class Home extends Component {
       <div>
         <img
           className="programming-image"
-          src={process.env.PUBLIC_URL + "/assets/programming.jpg"}
+          src={process.env.PUBLIC_URL + "/assets/space.jpg"}
         />
         <h2 className="home-description">
-          Hello, i am <b className="bolder-words"> Vedran</b> welcome to my site
+          Hello<b className="symbols">;</b> {"\n"} I am
+          <b className="bolder-words"> Vedran Brodar</b>
+          <b className="symbols">.</b>
+          {"\n"}
+          {"\n"}
+          <img
+            className="home-avatar"
+            src={process.env.PUBLIC_URL + "/assets/avatar.png"}
+          />
+          <h2 className="gradient-description">{"{ Frontend developer }"}</h2>
+          <h2 className="technologies-description">
+            HTML <b className="symbols">.</b> CSS(Sass)
+            <b className="symbols">.</b> Javascript <b className="symbols">.</b>{" "}
+            React{" "}
+          </h2>
         </h2>
       </div>
     );
